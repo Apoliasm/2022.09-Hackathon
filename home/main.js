8770775726f434c2345c4dee6caa458f4f8ae079
Junseok Park <pjs3859811@naver.com>
2022-09-25 01:57:51 +0900

    home_ver.2

--- home/main.js
+++ home/main.js
@@ -6,12 +6,15 @@
             level: 4 // 지도의 확대 레벨 
         };
     var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-    // 지도에 표시된 마커 객체를 가지고 있을 배열입니다
-    var markers = [];
-    var overlays = [];
+
+    var zoomControl = new kakao.maps.ZoomControl();
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+    var markers = []; // 마커 객체 리스트
+    var overlays = []; // 오버레이(info) 객체 리스트
     var clicked = false;
-    var locPosition;
-    
+
+    // markers 리스트 속 마커를 지도에 표시
     function setMarkers(map) {
         for (var i = 0; i < markers.length; i++) {
             if (markers[i] && currOver != overlays[i]) {
@@ -24,43 +27,70 @@
 
 //----------------------- GPS 사용 -----------------------
 {
+    var lat;
+    var lon;
+    var locPosition;
+    var mainMarker = null;
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 1000,
+        maximumAge: 0
+    };
+    var navi;
+    var start = false;
+
+    function success(position) {
+        lat = position.coords.latitude, // 위도
+            lon = position.coords.longitude; // 경도
+
+        locPosition = new kakao.maps.LatLng(lat, lon);
+        displayMarker(locPosition);
+        if (!start) { // 처음에만 중심 좌표로 이동
+            start = true;
+            moveCenter();
+        }
+    }
+
+    function error(err) {
+        console.log(err);
+    }
+
     // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
     if (navigator.geolocation) {
-
-        // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-        navigator.geolocation.getCurrentPosition(function (position) {
-
-            var lat = position.coords.latitude, // 위도
-                lon = position.coords.longitude; // 경도
-            locPosition = new kakao.maps.LatLng(lat, lon); // 인포윈도우에 표시될 내용입니다
-
-            // 마커와 인포윈도우를 표시합니다
-            displayMarker(locPosition);
-        });
-
-    } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-        alert('geolocation을 사용할수 없어요..');
+        navi = navigator.geolocation.watchPosition(success, error, options);
     }
 
     // 지도에 마커를 표시하는 함수입니다
     function displayMarker(locPosition) {
-        // 마커를 생성합니다
-        var marker = new kakao.maps.Marker({
+        if (mainMarker) {
+            mainMarker.setMap(null);
+        }
+        mainMarker = new kakao.maps.Marker({
             map: map,
             position: locPosition
         });
+
+        marker.setZIndex(0);
+
+        if (clicked) {
+            calcDistc();
+        }
+    }
+
+    // 지도 중심좌표를 접속위치로 변경합니다
+    function moveCenter() {
         if (currOver) {
             currOver.setMap(null);
         }
-        // 지도 중심좌표를 접속위치로 변경합니다
-        map.setCenter(locPosition);
-        if(clicked)
+        map.panTo(locPosition);
+        if (clicked) {
             calcDistc();
+        }
     }
 }
 
 
-//----------------------- DB 마커 표시 -----------------------
+//----------------------- test 마커 -----------------------
 {
     var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
     var imageSize = new kakao.maps.Size(24, 35);
@@ -83,8 +113,10 @@
     var content =
         '<div class="wrap">' +
         '    <div class="info">' +
-        '        <div class="title">쓰레기가 너무 많아요</div>' +
-        '        <button class="close" onclick="closeOverlay()" title="닫기"></button>' +
+        '        <div class="title">' +
+        '           <div class="text">쓰레기가 너무 많아요</div>' +
+        '           <button class="close" onclick="closeOverlay()" title="닫기"></button>' +
+        '        </div>' +
         '        <div class="body">' +
         '           <div class="image"><img src="https://img.etoday.co.kr/pto_db/2022/01/600/20220128134644_1713593_696_507.jpg" width="100%"></image></div>' +
         '           <button onclick="doDisplay()">해결하기</button>' +
@@ -92,21 +124,22 @@
         '   </div>' +
         '</div>';
 
-
     for (var i = 0; i < positions.length; i++) {
-        // 마커를 생성합니다
         var marker = new kakao.maps.Marker({
-            map: map, // 마커를 표시할 지도
-            position: positions[i].latlng, // 마커의 위치
-            image: markerImage // 마커 이미지
+            map: map,
+            position: positions[i].latlng,
+            image: markerImage,
         });
+        marker.setZIndex(1);
 
         var overlay = new kakao.maps.CustomOverlay({
             content: content,
             position: marker.getPosition()
         });
-        markers[i] = marker;
-        overlays[i] = overlay;
+        overlay.setZIndex(2);
+
+        markers.push(marker);
+        overlays.push(overlay);
 
         kakao.maps.event.addListener(marker, 'click', makeOutListener(overlay));
         kakao.maps.event.addListener(marker, 'click', makeOverListener(overlay));
@@ -139,14 +172,16 @@
 //----------------------- 플로깅 버튼 -----------------------
 {
     var clickLine = null,
-        distance = 0
-        menu = menu = document.querySelector('#ploggingWindow');
+        distance = 0,
+        menu = document.querySelector('#ploggingWindow');
 
-    function doDisplay(){
+    // 플로깅 창 띄우기
+    function doDisplay() {
         menu.classList.toggle('active');
         calcDistc();
     }
 
+    // 미터기 계산
     function calcDistc() {
         clickLine = new kakao.maps.Polyline({
             path: [locPosition, currOver.getPosition()]
@@ -159,32 +194,81 @@
         document.getElementById("distance").innerText = distance + 'm';
     }
 
+    // 확인 사진 첨부
+    function addImage() {
+        var completeImage = document.getElementById("addImage");
+        var frame = document.getElementById('frame');
+        var file;
+
+        completeImage.addEventListener("change", function (e) {
+            file = e.target.files[0];
+            frame.src = URL.createObjectURL(file);
+        });
+
+    }
+
     function complete() {
-        if(currOver){
+        if (currOver) {
             calcDistc();
-            if (distance < 200) {
-                //카메라찍음 사진 전송
-                swal("확인 후 포인트가 지급됩니다.");
+            if (distance < 20) {
+                document.getElementById("addImage").click();
+
+                var completeImage = document.getElementById("addImage");
+                var frame = document.getElementById('frame');
+                var file;
+
+                completeImage.addEventListener("change", function (e) {
+                    file = e.target.files[0];
+                    frame.src = URL.createObjectURL(file);
+                });
+
+                swal({
+                    text: "확인 후 포인트가 지급됩니다.",
+                    button: "확인",
+                });
                 markers[overlays.indexOf(currOver)].setMap(null);
                 markers[overlays.indexOf(currOver)] = null;
                 overlays[overlays.indexOf(currOver)] = null;
-                cancel();
+                menu.classList.toggle('active');
+                document.getElementById("address").innerText = "";
+                document.getElementById("distance").innerText = "";
+                distance = 0;
+                currOver = null;
+                clicked = false;
+                setMarkers(map);
             }
             else {
-                swal("해당 위치로 이동하세요.");
+                swal({
+                    text: "해당 위치로 이동하세요.",
+                    button: "확인",
+                });
             }
         }
     }
 
     function cancel() {
-        menu.classList.toggle('active');
-        document.getElementById("address").innerText = "";
-        document.getElementById("distance").innerText = "";
-        currOver = null;
-        clicked = false;
-        setMarkers(map);
+        swal({
+            text: "취소하시겠습니까?",
+            buttons: {
+                cancel: "아니요",
+                catch: {
+                    text: "예",
+                    value: "catch",
+                },
+            },
+        }).then((value) => {
+            if (value == "catch") {
+                menu.classList.toggle('active');
+                document.getElementById("address").innerText = "";
+                document.getElementById("distance").innerText = "";
+                distance = 0;
+                currOver = null;
+                clicked = false;
+                setMarkers(map);
+            }
+        })
     }
-    
+
     // 주소-좌표 변환 객체를 생성합니다
     var geocoder = new kakao.maps.services.Geocoder();
     function addresscheck() {
@@ -205,14 +289,82 @@
 
 //----------------------- 등록 버튼 -----------------------
 {
-    function addMarker() {
+    var camera = document.getElementById("add");
+    var frame = document.getElementById('frame');
+    var posting = document.querySelector('#postingWindow');
+    var file;
 
+    camera.addEventListener("change", function (e) {
+        file = e.target.files[0];
+        frame.src = URL.createObjectURL(file);
+        posting.classList.toggle('active');
+    });
+
+    function postingComplete() {
+        newMarker();
+        swal({
+            text: "등록되었습니다.",
+            button: "확인",
+        });
+        posting.classList.toggle('active');
+        document.getElementById("titleText").value = null;
+    }
+
+    function postingCancel() {
+        swal({
+            text: "취소하시겠습니까?",
+            buttons: {
+                cancel: "아니요",
+                catch: {
+                    text: "예",
+                    value: "catch",
+                },
+            },
+        }).then((value) => {
+            if (value == "catch") {
+                posting.classList.toggle('active');
+                document.getElementById("titleText").value = null;
+            }
+        })
+    }
+
+    // 새 마커 추가
+    function newMarker() {
+        var marker = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: locPosition, // 마커의 위치
+            image: markerImage // 마커 이미지
+        });
+        marker.setZIndex(1);
+
+        var content =
+            '<div class="wrap">' +
+            '    <div class="info">' +
+            '        <div class="title">' +
+            '           <div class="text">' + document.getElementById("titleText").value + '</div>' +
+            '           <button class="close" onclick="closeOverlay()" title="닫기"></button>' +
+            '        </div>' +
+            '        <div class="body">' +
+            '           <div class="image"><img src="' + URL.createObjectURL(file) + '" width="100%"></image></div>' +
+            '           <button onclick="doDisplay()">해결하기</button>' +
+            '       </div>' +
+            '   </div>' +
+            '</div>';
+
+        var overlay = new kakao.maps.CustomOverlay({
+            content: content,
+            position: marker.getPosition(),
+        });
+        overlay.setZIndex(2);
+
+        markers.push(marker);
+        overlays.push(overlay);
+
+        kakao.maps.event.addListener(marker, 'click', makeOutListener(overlay));
+        kakao.maps.event.addListener(marker, 'click', makeOverListener(overlay));
+
+        if (clicked) {
+            marker.setMap(null);
+        }
     }
 }
-
-
-
-
-
-
-
